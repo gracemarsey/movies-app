3f1a558ba5a0efb9526068e1d2aaa97c1b1aac4e
Brixton <brixton@MacBookAir.communityfibre.co.uk>
2025-09-14 19:48:45 +0100

    search

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,9 +1,25 @@
 import { FastifyInstance } from 'fastify';
 import { getUserByUsername } from '../services';
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const setupRoutes = async (fastify: FastifyInstance) => {
   fastify.get('/', async () => {
     return { message: 'Hello from the server!' };
+  });
+
+  fastify.get('/search/:movie', async (request, reply) => {
+    const { movie } = request.params as { movie: string };
+    const response = await axios.get(`http://www.omdbapi.com/?apikey=${process.env.VITE_OMDB_API_KEY}&t=${movie}`);
+
+    if (response.data.Response === "False") {
+      if (response.data.Error === "Movie not found!") {
+        return reply.status(404).send(response.data)
+      }
+    }
+
+    return reply.status(200).send(response.data);
   });
 
   fastify.get('/users/:username', async (request, reply) => {
